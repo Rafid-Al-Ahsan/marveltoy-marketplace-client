b36ee849606dc4d9c9955af31e56fa056b7d95f2
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2024-03-06 14:31:18 +0600

    ascending & descending added

--- src/components/Mytoy.jsx
+++ src/components/Mytoy.jsx
@@ -1,46 +1,73 @@
-import React, { useContext } from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { AuthContext } from '../provider/AuthProvider';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../routes/AuthProvider';
 import { Link } from 'react-router-dom';
 // React Rating
-import { Rating } from '@smastrom/react-rating'
-import '@smastrom/react-rating/style.css'
-
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css';
 
 const Mytoy = () => {
-
     const { user } = useContext(AuthContext);
     const [toys, setToys] = useState([]);
 
     useEffect(() => {
-        fetch(`http://localhost:5000/addtoy/email/${user.email}`)
-            .then(response => response.json())
-            .then(data => setToys(data))
-    }, [])
+        fetchToys();
+    }, []);
 
-    const handleDelete = id => {
+    const fetchToys = async () => {
+        const response = await fetch(`http://localhost:5000/addtoy/email/${user.email}`);
+        const data = await response.json();
+        setToys(data);
+    };
+
+    const handleDelete = async (id) => {
         console.log('deleted', id);
-    
-        fetch(`http://localhost:5000/addtoy/${id}`, {
+
+        const response = await fetch(`http://localhost:5000/addtoy/${id}`, {
             method: 'DELETE',
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            if(data.deletedCount>0) alert('deleted successfuly')
-            const remaing = toys.filter(toy => toy._id !== id)
-            setToys(remaing);
-        })
-    }
+        });
+        const data = await response.json();
+        console.log(data);
+
+        if (data.deletedCount > 0) {
+            alert('deleted successfuly');
+            const remaining = toys.filter((toy) => toy._id !== id);
+            setToys(remaining);
+        }
+    };
+
+    const handleSort = async (sortOrder) => {
+        let response;
+        if (sortOrder === 'ascending') {
+            response = await fetch(`http://localhost:5000/addtoy/email/ascending/${user.email}`);
+        } else if (sortOrder === 'descending') {
+            response = await fetch(`http://localhost:5000/addtoy/email/descending/${user.email}`);
+        }
+        else if (sortOrder === 'default') {
+            response = await fetch(`http://localhost:5000/addtoy/email/${user.email}`);
+        }
+        const data = await response.json();
+        setToys(data);
+    };
 
     return (
         <div>
             <p className='text-center my-5 text-xl font-bold'>Toys Added by: {user?.displayName}</p>
-            <div className='w-[90%] grid sm:grid-cols-1 md:grid-cols-2 m-auto'>
-                {toys.map(toy => <Card key={toy._id} toy={toy} handleDelete={handleDelete}></Card>)}
+            <div className='flex w-[95%] m-auto'>
+                <div className='w-[15%]'>
+                    <h1 className='text-xl'>Sort by Price</h1>
+                    <form>
+                        <input type="radio" id="ascending" name="sortOrder" value="ascending" onChange={() => handleSort('ascending')} />
+                        <label htmlFor="ascending"> Ascending</label><br/>
+                        <input type="radio" id="descending" name="sortOrder" value="descending" onChange={() => handleSort('descending')} />
+                        <label htmlFor="descending"> Descending</label><br/>
+                        <input type="radio" id="default" name="sortOrder" value="default" onChange={() => handleSort('default')} />
+                        <label htmlFor="default"> Default</label><br/>
+                    </form>
+                </div>
+                <div className='w-[85%] grid sm:grid-cols-1 md:grid-cols-2 m-auto'>
+                    {toys.map(toy => <Card key={toy._id} toy={toy} handleDelete={handleDelete}></Card>)}
+                </div>
             </div>
-
         </div>
     );
 };
@@ -57,15 +84,11 @@ function Card({ toy, handleDelete }) {
                 <h4 className='flex text-base text-[#fff]'>Subcategory: {toy.subcategory}</h4>
                 <p className='text-[#f4567e] italic'>{toy.description}</p>
                 <Rating style={{ maxWidth: 120 }} readOnly value={toy.rating} className="mt-2" />
-                {/* <div className="card-actions justify-start">
-                   <Link to={`herodetails/${toy.id}`}><button className="btn btn-primary bg-[#a3174f] px-10 mt-7">View Details</button></Link>
-                </div> */}
                 <div className='flex'>
                     <Link to={`/updatetoy/${toy._id}`}><button className='bg-[#0d6efd] h-10 px-8 mr-5 font-bold'>Update</button></Link>
-                    <button onClick={() => handleDelete(toy._id)} className='bg-[#dc3545] w-[20%] h-10 mr-5 font-bold'>Delete</button>
+                    <button onClick={() => handleDelete(toy._id)} className='bg-[#dc3545] px-8 h-10 mr-5 font-bold'>Delete</button>
                 </div>
             </div>
         </div>
-
     )
 }
